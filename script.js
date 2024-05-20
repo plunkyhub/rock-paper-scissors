8f1ddbb7c1015a417043ed77635ab7cccfbbf21d
Plunkyhub <165522642+plunkyhub@users.noreply.github.com>
2024-05-20 11:45:32 -0400

    Creadas las constantes eleccionJugador y eleccionPc que apuntan a las clases eleccionJugador y eleccionPc en el index.html

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const papel = document.querySelector("#papel");
 const tijera = document.querySelector("#tijera");
 //opcion empate
 const empate = document.querySelector(".empate");
+//Apuntar a los selectores de eleccion
+const eleccionJugador = document.querySelector(".eleccionJugador");
+const eleccionPc = document.querySelector(".eleccionPc");
