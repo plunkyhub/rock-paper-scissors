fa07f3c5c1630f9d31ab587b48d7aed5ff2c5ae4
Plunkyhub <165522642+plunkyhub@users.noreply.github.com>
2024-05-20 11:43:02 -0400

    Creada la constante empate que apunta a la clase empate

--- script.js
+++ script.js
@@ -2,3 +2,5 @@
 const piedra = document.querySelector("#piedra");
 const papel = document.querySelector("#papel");
 const tijera = document.querySelector("#tijera");
+//opcion empate
+const empate = document.querySelector(".empate");
