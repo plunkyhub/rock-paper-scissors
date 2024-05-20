86d7b76bc371d07cca94a712a1d547926c56438c
Plunkyhub <165522642+plunkyhub@users.noreply.github.com>
2024-05-20 11:50:38 -0400

    Creadas las constantes jugadorPuntos y pcPuntos que apuntan a los id jugadorPuntos y pcPuntos. A las constantes jugadorPuntos y pcPuntos se les asigno el textContent de 0 en index.html

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ const empate = document.querySelector(".empate");
 //Apuntar a los selectores de eleccion
 const eleccionJugador = document.querySelector(".eleccionJugador");
 const eleccionPc = document.querySelector(".eleccionPc");
+//Mostrar puntuacion jugador y computadora
+const jugadorPuntos = document.querySelector("#jugadorPuntos");
+jugadorPuntos.textContent = 0;
+const pcPuntos = document.querySelector("#pcPuntos");
+pcPuntos.textContent = 0;
